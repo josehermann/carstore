8195989614baa4ae34698f4fee5db9d7c9d71af3
unknown <imthelaw115@gmail.com>
2023-08-01 13:32:33 +0200

    page articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,13 @@ class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
     //* une route est définie par 2 arguments : son chemin (/blog) dans l'url et son nom (app_blog)
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        //*$repo est instance de la classe ArticleRepository et possède du cout les 4 méthodes de bases find(), findOneBy(), findAll(), findBy()
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            "articles" =>  $articles
         ]);
         //* render() permet d'afficher le contenu d'un template. Elle va chercher directement dans le dossier template
     }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,13 +2,25 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Article;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        for($i=1; $i <= 15; $i++)
+        {
+            $article = new Article;
+
+            $article->setTitle("Titre de l'article n°$i")
+                    ->setImage("https://picsum.photos/200/300")
+                    ->setContent("<p>Contenu de l'article n°$i</p>")
+                    ->setCreatedAt(new \Datetime());
+                    
+            $manager->persist($article);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
